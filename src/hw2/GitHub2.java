e0d1c01ed877a5ab5215feb697a117f38cf6a7f1
brett <brett@DonLish>
2025-01-30 09:08:10 -0500

    CreateRepository

---
+++ src/hw2/GitHub2.java
@@ -0,0 +1,31 @@
+package hw2;
+
+public class GitHub2 {
+
+public static void main(String[] args) {
+		
+		String s1 = "JAVA";
+		String s2 = "JAVA";
+		
+		/**
+		 * Write if-else condition to check if s1 equals s2
+		 */
+		if(s1.equals(s2)) {
+			System.out.println("Equals");
+		} else {
+			System.out.println("Not Equals");
+		}
+		
+		String s3 = new String("JAVA");
+		String s4 = new String("JAVA");
+		
+		/**
+		 * Write if-else condition to check if s3 equals s4
+		 */
+		if(s3.equals(s4)) {
+			System.out.println("Equals");
+		} else {
+			System.out.println("Not Equals");
+		}
+	}
+}
